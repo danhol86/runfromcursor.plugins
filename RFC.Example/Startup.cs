787ceabd2c820c08464ab35ca3ce889a9fa26a39
Dan Holgate <Dan.Holgate@agileautomations.co.uk>
2024-01-06 21:18:54 +0000

    ....

--- RFC.Example/Startup.cs
+++ RFC.Example/Startup.cs
@@ -29,13 +29,13 @@ namespace LinqPad1
         //First screen xaml
         public static string GetXaml()
         {
-            return ConvertUserControlToXamlString(new UserControl1());
+            return Helpers.ConvertUserControlToXamlString(new UserControl1());
         }
 
         //Name of plugin
         public static string GetControlName()   
         {
-            return "Simple Commit 3";
+            return "Simple Commit 5";
         }
 
         //Deals with button clicks, and must return a new screen xaml. or null. which leaves as is
@@ -43,7 +43,7 @@ namespace LinqPad1
         {
             var FieldValues = JsonConvert.DeserializeObject<Dictionary<string, string>>(FieldValuesAsJson); 
 
-            var dte = DteFinder.GetAllDtes(myrot);
+            var dte = Helpers.GetAllDtes(myrot);
 
             return RunPlugin(dte, FieldValues);
         }
@@ -57,19 +57,18 @@ namespace LinqPad1
 
         static string RunPlugin(DTE dte, Dictionary<string, string> FieldValues)
         {
-            System.Diagnostics.Debugger.Break();
             try
             {
                 var commitm = FieldValues["MyTextBox"];
 
-                GitPlugin.Run(commitm, dte); // Assuming GitTest is a synchronous method
+                GitPlugin.Run(commitm, dte);
 
                 var ucontrol = new UserControl1();
 
                 ucontrol.MyMessage.Visibility = Visibility.Visible;
                 ucontrol.MyLabel.Text = "Commited - " + DateTime.Now.ToString();
 
-                string result = ConvertUserControlToXamlString(ucontrol);
+                string result = Helpers.ConvertUserControlToXamlString(ucontrol);
                 return result;
             }
             catch (Exception e)
@@ -78,71 +77,72 @@ namespace LinqPad1
 
                 errorc.MyLabel.Text = "Error - " + e.ToString();
 
-                string result = ConvertUserControlToXamlString(errorc);
+                string result = Helpers.ConvertUserControlToXamlString(errorc);
                 return result;
             }
         }
         
-        public static string ConvertUserControlToXamlString(UserControl userControl)
-        {
-            StringBuilder sb = new StringBuilder();
-            XmlWriterSettings settings = new XmlWriterSettings
-            {
-                Indent = true,
-                OmitXmlDeclaration = true
-            };
-
-            using (XmlWriter xmlWriter = XmlWriter.Create(sb, settings))
-            {
-                XamlWriter.Save(userControl, xmlWriter);
-            }
-
-            return sb.ToString();
-        }
+        
     }
+}
 
-
-    public static class DteFinder
+public static class Helpers
+{
+    public static string ConvertUserControlToXamlString(UserControl userControl)
     {
-        public static DTE GetAllDtes(string RotName)
+        StringBuilder sb = new StringBuilder();
+        XmlWriterSettings settings = new XmlWriterSettings
         {
-            IntPtr numFetched = Marshal.AllocHGlobal(sizeof(int));
+            Indent = true,
+            OmitXmlDeclaration = true
+        };
 
-            IRunningObjectTable runningObjectTable;
-            IEnumMoniker monikerEnumerator;
-            IMoniker[] monikers = new IMoniker[1];
-
-
-            IBindCtx bindCtx;
-            Marshal.ThrowExceptionForHR(CreateBindCtx(reserved: 0, ppbc: out bindCtx));
-
-            bindCtx.GetRunningObjectTable(out runningObjectTable);
-
-            runningObjectTable.EnumRunning(out monikerEnumerator);
-            monikerEnumerator.Reset();
-
-            while (monikerEnumerator.Next(1, monikers, numFetched) == 0)
-            {
-                IBindCtx ctx;
-                CreateBindCtx(0, out ctx);
-
-                string runningObjectName;
-                monikers[0].GetDisplayName(ctx, null, out runningObjectName);
-
-                if (runningObjectName.Contains("VisualStudio.DTE.") && runningObjectName == RotName)
-                {
-                    object runningObjectVal;
-                    runningObjectTable.GetObject(monikers[0], out runningObjectVal);
-
-                    DTE dte = (DTE)runningObjectVal;
-
-                    return dte;
-                }
-            }
-            return null;
+        using (XmlWriter xmlWriter = XmlWriter.Create(sb, settings))
+        {
+            XamlWriter.Save(userControl, xmlWriter);
         }
 
-        [DllImport("ole32.dll")]
-        private static extern int CreateBindCtx(uint reserved, out IBindCtx ppbc);
+        return sb.ToString();
     }
+
+    public static DTE GetAllDtes(string RotName)
+    {
+        IntPtr numFetched = Marshal.AllocHGlobal(sizeof(int));
+
+        IRunningObjectTable runningObjectTable;
+        IEnumMoniker monikerEnumerator;
+        IMoniker[] monikers = new IMoniker[1];
+
+
+        IBindCtx bindCtx;
+        Marshal.ThrowExceptionForHR(CreateBindCtx(reserved: 0, ppbc: out bindCtx));
+
+        bindCtx.GetRunningObjectTable(out runningObjectTable);
+
+        runningObjectTable.EnumRunning(out monikerEnumerator);
+        monikerEnumerator.Reset();
+
+        while (monikerEnumerator.Next(1, monikers, numFetched) == 0)
+        {
+            IBindCtx ctx;
+            CreateBindCtx(0, out ctx);
+
+            string runningObjectName;
+            monikers[0].GetDisplayName(ctx, null, out runningObjectName);
+
+            if (runningObjectName.Contains("VisualStudio.DTE.") && runningObjectName == RotName)
+            {
+                object runningObjectVal;
+                runningObjectTable.GetObject(monikers[0], out runningObjectVal);
+
+                DTE dte = (DTE)runningObjectVal;
+
+                return dte;
+            }
+        }
+        return null;
+    }
+
+    [DllImport("ole32.dll")]
+    private static extern int CreateBindCtx(uint reserved, out IBindCtx ppbc);
 }
